ee61ed8b398b48d0ac0b3e4526d2c1bc9c6837ca
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2022-01-26 01:48:11 -0300

    first commit

---
+++ carrinho_compras.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+List<String> produtos = [];
+
+main() {
+  bool condicao = true;
+
+  while (condicao) {
+    print("==== ADICIONE UM PRODUTO ====");
+    String text = stdin.readLineSync();
+    if (text == "sair") {
+      print("===== TERMINOU O PROGRAMA =====");
+      condicao = false;
+    } else if (text == "imprimir") {
+      imprimir();
+    } else if (text == "remover") {
+      remover();
+    } else {
+      produtos.add(text);
+      print("\x1B[2J\x1B[0;0H");
+    }
+  }
+}
+
+imprimir() {
+  for (var i = 0; i < produtos.length; i++) {
+    print("ITEM $i - ${produtos[i]}");
+  }
+}
+
+remover() {
+  print("=== QUAL ITEM DESEJA REMOVER?? ===");
+  for (var i = 0; i < produtos.length; i++) {
+    print("ITEM $i - ${produtos[i]}");
+  }
+  int item = int.parse(stdin.readLineSync());
+  produtos.removeAt(item);
+  print("ITEM REMOVIDO");
+}
